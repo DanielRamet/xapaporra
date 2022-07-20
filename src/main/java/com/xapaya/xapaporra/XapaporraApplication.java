eafb835cbfc08a715e23a72637f0f6a12e0b67f5
Xapaya <drametbarea@gmail.com>
2022-07-20 12:43:00 +0200

    Initial commit

---
+++ src/main/java/com/xapaya/xapaporra/XapaporraApplication.java
@@ -0,0 +1,13 @@
+package com.xapaya.xapaporra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XapaporraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XapaporraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xapaya/xapaporra/XapaporraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xapaya.xapaporra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XapaporraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
