79c7339e5ea44c4e2598b328f35b103592673141
Xapaya <drametbarea@gmail.com>
2022-12-26 00:23:00 +0100

    Add test endpoint

--- src/main/java/com/xapaya/XapaporraApplication.java
+++ src/main/java/com/xapaya/XapaporraApplication.java
@@ -4,9 +4,11 @@ import com.xapaya.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
 public class XapaporraApplication {
