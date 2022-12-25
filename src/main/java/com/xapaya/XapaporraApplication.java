43a67ef7ea30fedf3049a436eb6bd7c9b2d2d98d
Xapaya <drametbarea@gmail.com>
2022-12-26 00:22:01 +0100

    Add test endpoint

--- src/main/java/com/xapaya/XapaporraApplication.java
+++ src/main/java/com/xapaya/XapaporraApplication.java
@@ -4,14 +4,18 @@ import com.xapaya.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-@EnableScheduling
-@EnableAsync
 public class XapaporraApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(XapaporraApplication.class, args);
